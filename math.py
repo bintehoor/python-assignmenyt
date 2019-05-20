9b39ca52e9d42e90de077085dd52b96645641d24
bintehoor <50301178+bintehoor@users.noreply.github.com>
2019-05-20 15:51:34 +0500

    assignment

---
+++ math.py
@@ -0,0 +1,31 @@
+#ANSWERS OF QUESTION[13,16,18,22]
+from math import  sqrt
+'''print("-----------------------------%(HYPOTENUSE OF RIGHT ANGLE TRIANGLE)%-------------------------------")
+print("Enter the length:-")
+a=float(input("a : "))
+b=float(input("b : "))
+c=sqrt(a**2 + b**2)
+print("The Lenght Of The Hypotenuse is",c)
+
+print("-----------------------------%(DISTANCE B/W TWO POINTS(X1,X2)(Y1,Y2)%-------------------------------")
+x1 =int(input("ENTER VALUE FOR X1 POINT: "))
+y1 =int(input("ENTER VALUE FOR Y1 POINT: "))
+x2 =int(input("ENTER VALUE FOR X2 POINT: "))
+y2 =int(input("ENTER VALUE FOR Y2 POINT: "))
+distance=sqrt((x1-x2)**2 + (y1-y2)**2)
+print("DISTANCE B/W TWO POINTS = ",distance)
+
+print("-----------%(TWO INTEGERS VALUES IS EQUALS,VALUES ADDITION AND VALUES SUBTRACTION IS 5)%---------------")
+num_int1 = int(input("ENTER 1ST INTEGER NUMBER : "))
+num_int2 = int(input("ENTER 2ND INTEGER NUMBER : "))
+if (num_int1 == num_int2) | (num_int1+num_int2 == 5) | (num_int1-num_int2 == 5):
+    print("TRUE")
+else:
+    print("NOT TRUE")'''
+
+print("-----------------------------%(CALCULATE BODY MASS INDEX(BMD))%-------------------------------")
+w=float(input("ENTER WEIGHT IN KILOGRAM(Kg): "))
+h = float(input("ENTER HEIGHT IN METERS(m): "))
+bmi=w/(h*h)
+print("BODY MASS INDEX(BMI)= ",bmi)
+

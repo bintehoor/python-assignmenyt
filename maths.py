00c24208b73b298f6853305398a96996b0617b8f
bintehoor <50301178+bintehoor@users.noreply.github.com>
2019-05-20 12:45:49 +0500

    ASSIGNMENT

---
+++ maths.py
@@ -0,0 +1,28 @@
+# THIS IS THE ANSWERS OF PYTHON ASSIGNMENT QUESTION : [1,4,6,12,14]
+
+r = int(input("ENTER RADIOUS FOR 'AREA OF CIRCLE' : "))
+radous = int(input("ENTER RADIOUS FOR VOLIUM OF TRIANGLE' : "))
+h = int(input("ENTER HEIGHT FOR 'AREA OF TRIANGLE' : "))
+b = int(input("ENTER BASE FOR AREA OF TRIANGLE' : "))
+n = int(input("ENTER ANY INTEGER NUMBER : "))
+x = int(input("ENTER ANY NUMBER FOR X : "))
+y = int(input("ENTER ANY  NUMBER FOR Y : "))
+pi = 3.14
+print("----------------------------------%(AREA OF CIRCLE)%--------------------------------------------")
+area = pi**r
+print("AREA OF CIRCLE : A = ",area)
+print("----------------------------------%(VOLIUM OF SPHERE)%-------------------------------------------")
+volium = 4/3*pi*radous**radous
+print("VOLIUM OF SPHERE : V = ",volium)
+print("----------------------------------%(AREA OF TRIANGLE)%--------------------------------------------")
+area_t = 1/2*b*h
+print("AREA OF TRIANGLE : A = ",area_t)
+print("----------------------------------%(VALUE OF INTEGER(n) : (n+nn+nnn))%-----------------------------")
+num = n+n*n+n**n
+print("VALUE OF INTEGER(n) : (n+nn+nnn) = ",num)
+print("----------------------------------%(SOLUTION : (x+y)*(x+y))%----------------------------------------")
+f = (x+y)*(x+y)
+print("SOLUTION : (x+y)*(x+y) =  ",f)
+
+
+
